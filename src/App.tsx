632894c8cbd6c1107d85b645ec53c016b522608b
VictorHugoBN <hugobn.victor@gmail.com>
2024-06-27 14:53:36 -0300

    add prettier configs and eslint configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header />
+      <Header></Header>
       <Outlet />
     </>
   );
