b1e6a4f33574c5c6f4216f5b3d0eacf6afca13a9
VictorHugoBN <hugobn.victor@gmail.com>
2024-11-21 14:16:47 -0300

    fix tsc errors

--- src/components/Home/SelosDeQualidade.tsx
+++ src/components/Home/SelosDeQualidade.tsx
@@ -1,47 +1,4 @@
-import { makeStyles } from "@mui/styles";
-import { theme } from "../../Style/Theme";
-
-const useStyles = makeStyles(() => ({
-  containerPrincipal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-  },
-  text1: {
-    fontSize: "1.6rem",
-    fontWeight: 400,
-    color: theme.palette.primary.contrastText,
-  },
-  divCards: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: "2rem",
-    marginTop: "2rem",
-    flexWrap: "wrap",
-    width: "100%",
-  },
-  card: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "230px",
-    height: "300px",
-    borderRadius: "10px",
-    padding: "1rem",
-    border: "1px solid black",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    [theme.breakpoints.down("sm")]: {
-      width: "85%",
-      height: "200px",
-    },
-  },
-}));
-
 const SelosDeQualidade = () => {
-  const classes = useStyles();
   return null;
 
   /*   return (
